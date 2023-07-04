6f5074d234f00446afda7ea3cb860aebf5ab043c
sateeshlevelup <112386309+sateeshlevelup@users.noreply.github.com>
2023-07-04 23:20:11 +0530

    Update app.py

--- Compose/app.py
+++ Compose/app.py
@@ -20,4 +20,4 @@ def get_hit_count():
 @app.route('/')
 def hello():
     count = get_hit_count()
-    return 'Hello World! I have been seen {} times.\n'.format(count)
+    return 'Welcome to LevelUp! I refreshed this page for  {} times.\n'.format(count)
